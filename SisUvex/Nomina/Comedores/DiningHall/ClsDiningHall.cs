ccb9d9e58d6ed73fa21e306243ce90fb8c96fe78
Daniel <dannyallson00@gmail.com>
2024-12-21 11:27:35 -0700

    FrmDiningHall

---
+++ SisUvex/Nomina/Comedores/DiningHall/ClsDiningHall.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisUvex.Nomina.Comedores.DiningHall
+{
+    internal class ClsDiningHall
+    {
+    }
+}
