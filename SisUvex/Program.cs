c7a3ca1c08c65a4fe15eda53f26e34b9c47b930f
susana <susanaaguirre717@gmail.com>
2026-03-02 12:17:29 -1200

    Despues del merge para juntar cambios de Nomina Empaque y Cambios Dani de reporte de plantas y contrato

--- SisUvex/Program.cs
+++ SisUvex/Program.cs
@@ -54,7 +54,7 @@ namespace SisUvex
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             //Application.Run(new FrmManifestCat());
-            Application.Run(new FrmMenu());
+            Application.Run(new PantallaCarga());
 
         }
     }
