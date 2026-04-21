56134ffa39d0f8c53ef13f1816963c0aff116713
susana <susanaaguirre717@gmail.com>
2026-04-21 14:50:45 -0700

    se creo un fromulario para el tabulador

---
+++ SisUvex/Nomina/NomTabulador/ClsNomTabuladorCat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisUvex.Nomina.NomTabulador
+{
+	internal class ClsNomTabuladorCat
+	{
+	}
+}
