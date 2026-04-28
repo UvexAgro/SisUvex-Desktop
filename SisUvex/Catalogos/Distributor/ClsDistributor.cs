5258a9639cecd90fa7ecec5ebaf2e47c55538ebc
DANIEL LIMON <dannyallson00@gmail.com>
2026-04-27 18:36:46 -0700

    +Nuevo formularios y clases para distributor, (solo plantilla sin codigo)

---
+++ SisUvex/Catalogos/Distributor/ClsDistributor.cs
@@ -0,0 +1,73 @@
+﻿using DocumentFormat.OpenXml.Presentation;
+using SisUvex.Catalogos.Metods.ComboBoxes;
+using SisUvex.Catalogos.Metods.Controls;
+using SisUvex.Catalogos.Metods.DataGridViews;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SisUvex.Catalogos.Metods.ClsObject;
+
+namespace SisUvex.Catalogos.Distributor
+{
+    internal class ClsDistributor
+    {
+        ClsControls controlList = null!;
+        public FrmDistributorAdd _frmAdd = null!;
+        public FrmDistributorCat _frmCat = null!;
+        public EDistributor entity = null!;
+
+        private readonly string queryCatalog = $" SELECT cat.* FROM vw_PackDistributorCat cat ";
+
+        ClsDGVCatalog? dgv;
+        DataTable dtCatalog = null!;
+
+        public bool IsAddOrModify = true;
+        public bool IsAddUpdate;
+        public bool IsModifyUpdate;
+        public string? idAddModify;
+        private void AddControlsToList()
+        {
+            controlList = new ClsControls();
+
+            controlList.ChangeHeadMessage("Para dar de alta un distribuidor debe:\n");
+            controlList.Add(_frmAdd.txbId, "Ingresar el ID del distribuidor.");
+            controlList.Add(_frmAdd.txbName, "Ingresar el nombre del distribuidor.");
+            controlList.Add(_frmAdd.txbShortName, "Ingresar el nombre corto del distribuidor.");
+            controlList.Add(_frmAdd.cboActive, "Seleccionar si el distribuidor está activo.");
+        }
+
+        private void LoadControls()
+        {
+            ClsComboBoxes.CboLoadActives(_frmAdd.cboAgencyUS, AgencyTradeUS.Cbo);
+            ClsComboBoxes.CboLoadActives(_frmAdd.cboAgencyMX, AgencyTradeMX.Cbo);
+            ClsComboBoxes.CboLoadActives(_frmAdd.cboGrower, Grower.Cbo);
+            ClsComboBoxes.CboLoadActives(_frmAdd.cboCityCross, City.Cbo);
+            ClsComboBoxes.CboLoadActives(_frmAdd.cboCityDestination, City.Cbo);
+
+            ClsComboBoxes.CboApplyClickEvent(_frmAdd.cboAgencyUS, _frmAdd.chbAgencyUSRemoved);
+            ClsComboBoxes.CboApplyClickEvent(_frmAdd.cboAgencyMX, _frmAdd.chbAgencyMXRemoved);
+            ClsComboBoxes.CboApplyClickEvent(_frmAdd.cboGrower, _frmAdd.chbGrowerRemoved);
+            ClsComboBoxes.CboApplyClickEvent(_frmAdd.cboCityCross, _frmAdd.chbCityCrossRemoved);
+            ClsComboBoxes.CboApplyClickEvent(_frmAdd.cboCityDestination, _frmAdd.chbCityDestinationRemoved);
+        }
+        private void LoadControlsModify()
+        {
+            //AQUI LOS QUE DEBAN DE DISTRIBUTOR
+            //entity = new();
+            //entity.GetWorkGroup(idAddModify);
+
+            //_frmAdd.txbId.Text = entity.IdWorkGroup ?? "";
+            //_frmAdd.txbName.Text = entity.NameWorkGroup ?? "";
+            //_frmAdd.txbIdContractor.Text = entity.IdContractor ?? "";
+            //_frmAdd.txbIdSeason.Text = entity.IdSeason ?? "";
+            //_frmAdd.cboActive.SelectedIndex = entity.Active;
+
+
+            //PERO CARGAR LOS COMBOBOX ASI DISTRIBUTOR
+            //ClsComboBoxes.CboSelectIndexWithTextInValueMember(_frmAdd.cboAgencyUS, entity.idAgencyUS);
+            //Y ASI LOS DEMAS COMBOBOX
+        }
+    }
+}
---
+++ SisUvex/Catalogos/Distributor/FrmDistributorCat.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace SisUvex.Catalogos.Distributor
+{
+    partial class FrmDistributorCat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDistributorCat));
+            chbRemoved = new CheckBox();
+            dgvCatalog = new DataGridView();
+            btnRemove = new Button();
+            btnRecover = new Button();
+            btnModify = new Button();
+            btnAdd = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvCatalog).BeginInit();
+            SuspendLayout();
+            // 
+            // chbRemoved
+            // 
+            chbRemoved.Appearance = Appearance.Button;
+            chbRemoved.Location = new Point(174, 12);
+            chbRemoved.Name = "chbRemoved";
+            chbRemoved.Size = new Size(75, 23);
+            chbRemoved.TabIndex = 8;
+            chbRemoved.Text = "Eliminados";
+            chbRemoved.TextAlign = ContentAlignment.MiddleCenter;
+            chbRemoved.UseVisualStyleBackColor = true;
+            // 
+            // dgvCatalog
+            // 
+            dgvCatalog.AllowUserToAddRows = false;
+            dgvCatalog.AllowUserToDeleteRows = false;
+            dgvCatalog.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvCatalog.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgvCatalog.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvCatalog.BackgroundColor = SystemColors.ControlLightLight;
+            dgvCatalog.BorderStyle = BorderStyle.Fixed3D;
+            dgvCatalog.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dgvCatalog.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dgvCatalog.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dgvCatalog.EnableHeadersVisualStyles = false;
+            dgvCatalog.ImeMode = ImeMode.NoControl;
+            dgvCatalog.Location = new Point(12, 41);
+            dgvCatalog.Name = "dgvCatalog";
+            dgvCatalog.ReadOnly = true;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dgvCatalog.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            dgvCatalog.RowHeadersVisible = false;
+            dgvCatalog.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            dgvCatalog.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvCatalog.Size = new Size(573, 397);
+            dgvCatalog.TabIndex = 11;
+            // 
+            // btnRemove
+            // 
+            btnRemove.Location = new Point(255, 12);
+            btnRemove.Name = "btnRemove";
+            btnRemove.Size = new Size(75, 23);
+            btnRemove.TabIndex = 9;
+            btnRemove.Text = "Eliminar";
+            btnRemove.UseVisualStyleBackColor = true;
+            // 
+            // btnRecover
+            // 
+            btnRecover.Location = new Point(336, 12);
+            btnRecover.Name = "btnRecover";
+            btnRecover.Size = new Size(75, 23);
+            btnRecover.TabIndex = 10;
+            btnRecover.Text = "Recuperar";
+            btnRecover.UseVisualStyleBackColor = true;
+            // 
+            // btnModify
+            // 
+            btnModify.Location = new Point(93, 12);
+            btnModify.Name = "btnModify";
+            btnModify.Size = new Size(75, 23);
+            btnModify.TabIndex = 7;
+            btnModify.Text = "Modificar";
+            btnModify.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(12, 12);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(75, 23);
+            btnAdd.TabIndex = 6;
+            btnAdd.Text = "Añadir";
+            btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // FrmDistributorCat
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(597, 450);
+            Controls.Add(chbRemoved);
+            Controls.Add(dgvCatalog);
+            Controls.Add(btnRemove);
+            Controls.Add(btnRecover);
+            Controls.Add(btnModify);
+            Controls.Add(btnAdd);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "FrmDistributorCat";
+            StartPosition = FormStartPosition.CenterParent;
+            Text = "FrmDistributorCat";
+            WindowState = FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)dgvCatalog).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        public CheckBox chbRemoved;
+        public DataGridView dgvCatalog;
+        private Button btnRemove;
+        private Button btnRecover;
+        private Button btnModify;
+        private Button btnAdd;
+    }
+}
