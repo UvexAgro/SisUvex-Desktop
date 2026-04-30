f1ce29279d3350cbdeb506473e9afb2b56c79bee
susana <susanaaguirre717@gmail.com>
2026-04-30 11:49:28 -0700

    frm para reporte de horas y cajas empacadas

---
+++ SisUvex/Nomina/Reporte de Emp UVA/FrmUva.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI.SS.Formula.Functions;
+using static SisUvex.Catalogos.Metods.ClsObject;
+
+namespace SisUvex.Nomina.Reporte_de_Emp_UVA
+{
+	public partial class FrmUva : Form
+	{
+		public ClsUva cls;
+		public bool cargando = false;
+		public FrmUva()
+		{
+			InitializeComponent();
+		}
+
+		private void FrmUva_Load(object sender, EventArgs e)
+		{
+
+			cls ??= new();
+			cls.frm ??= this;
+		}
+
+		private void btnCargar_Click(object sender, EventArgs e)
+		{
+			cls.CargarHorasInicial();
+		}
+
+		private void btnExcel_Click(object sender, EventArgs e)
+		{
+			cls.ExportarExcel((DataTable)dgvHoras.DataSource);
+		}
+	}
+}
