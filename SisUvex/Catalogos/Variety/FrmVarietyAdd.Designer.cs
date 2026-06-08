511f691af9336bad29d0eddf929561e3db29a3e8
oswaldo <oswaldo.gutierrez@uvex.com.mx>
2026-06-07 17:59:22 -0700

    cambiesillos2

--- SisUvex/Catalogos/Variety/FrmVarietyAdd.Designer.cs
+++ SisUvex/Catalogos/Variety/FrmVarietyAdd.Designer.cs
@@ -89,7 +89,7 @@ partial class FrmVarietyAdd
         // 
         txbNameComercial.Font = new Font("Segoe UI", 12F);
         txbNameComercial.Location = new Point(169, 82);
-        txbNameComercial.MaxLength = 20;
+        txbNameComercial.MaxLength = 60;
         txbNameComercial.Name = "txbNameComercial";
         txbNameComercial.Size = new Size(363, 29);
         txbNameComercial.TabIndex = 4;
--- SisUvex/Nomina/Work time/FrmWorkTimeCat.cs
+++ SisUvex/Nomina/Work time/FrmWorkTimeCat.cs
@@ -12,128 +12,133 @@ using SisUvex.Catalogos.Metods;
 
 namespace SisUvex.Nomina.Work_time
 {
-	internal partial class FrmWorkTimeCat : Form
-	{
-		bool cambiando = false;
-		bool isLoaded = false;
-		ClsWorkTime cls;
-		public Color colorComida = Color.FromArgb(200, 225, 255);
-		public Color colorCena = Color.FromArgb(220, 235, 255);
-		public Color colorDescanso = Color.FromArgb(240, 245, 255);
-		public Color colorDescanso2 = Color.FromArgb(225, 240, 255);
+    internal partial class FrmWorkTimeCat : Form
+    {
+        bool cambiando = false;
+        bool isLoaded = false;
+        ClsWorkTime cls;
+        public Color colorComida = Color.FromArgb(200, 225, 255);
+        public Color colorCena = Color.FromArgb(220, 235, 255);
+        public Color colorDescanso = Color.FromArgb(240, 245, 255);
+        public Color colorDescanso2 = Color.FromArgb(225, 240, 255);
 
-		public FrmWorkTimeCat(ClsWorkTime clsClass)
-		{
-			InitializeComponent();
-			cls = (clsClass);
-		}
+        public FrmWorkTimeCat(ClsWorkTime clsClass)
+        {
+            InitializeComponent();
+            cls = (clsClass);
+        }
 
-		private void btnAdd_Click(object sender, EventArgs e)
-		{
-			cls.OpenFrmAdd();
-		}
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            cls.OpenFrmAdd();
+        }
 
-		private void btnModify_Click(object sender, EventArgs e)
-		{
-			cls.OpenFrmModify();
-		}
+        private void btnModify_Click(object sender, EventArgs e)
+        {
+            cls.OpenFrmModify();
+        }
 
-		private void FrmWorkTimeCat_Load(object sender, EventArgs e)
-		{
-			cls.BeginFormCat();
-			cls.CargarTemporada();
+        private void FrmWorkTimeCat_Load(object sender, EventArgs e)
+        {
+            cls.BeginFormCat();
+            cls.CargarTemporada();
 
-			dgvCatalog.CellPainting += dgvCatalog_CellPainting;
+            dgvCatalog.CellPainting += dgvCatalog_CellPainting;
 
-			dgvCatalog.DataBindingComplete -= dgvCatalog_DataBindingComplete;
-			dgvCatalog.DataBindingComplete += dgvCatalog_DataBindingComplete;
+            dgvCatalog.DataBindingComplete -= dgvCatalog_DataBindingComplete;
+            dgvCatalog.DataBindingComplete += dgvCatalog_DataBindingComplete;
 
-			isLoaded = true;
+            isLoaded = true;
 
-			cls.LoadDgvCatalog();
-		}
+            cls.LoadDgvCatalog();
+        }
 
-		private void btnSearch_Click(object sender, EventArgs e)
-		{
-			cls.LoadDgvCatalog();
-		}
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            cls.LoadDgvCatalog();
+        }
 
-		private void dgvCatalog_DoubleClick(object sender, EventArgs e)
-		{
-			cls.OpenFrmModify();
-		}
+        private void dgvCatalog_DoubleClick(object sender, EventArgs e)
+        {
+            cls.OpenFrmModify();
+        }
 
-		private void btnReportField_Click(object sender, EventArgs e)
-		{
-			cls.OpenFrmReport(1); //1 PARA REPORTE EN PRECIOS DE CAMPO
-		}
+        private void btnReportField_Click(object sender, EventArgs e)
+        {
+            cls.OpenFrmReport(1); //1 PARA REPORTE EN PRECIOS DE CAMPO
+        }
 
-		private void btnReportFacility_Click(object sender, EventArgs e)
-		{
-			cls.OpenFrmReport(2); //2 PARA REPORTE CON PRECIOS DE EMPAQUE
-		}
+        private void btnReportFacility_Click(object sender, EventArgs e)
+        {
+            cls.OpenFrmReport(2); //2 PARA REPORTE CON PRECIOS DE EMPAQUE
+        }
 
-		private void dgvCatalog_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
-		{
-			cls.PintarEncabezadosAgrupados((DataGridView)sender, e);
-		}
+        private void dgvCatalog_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            cls.PintarEncabezadosAgrupados((DataGridView)sender, e);
+        }
 
-		private void cboFechaInicio_SelectedIndexChanged(object sender, EventArgs e)
-		{
+        private void cboFechaInicio_SelectedIndexChanged(object sender, EventArgs e)
+        {
 
-			if (!isLoaded || cambiando) return;
+            if (!isLoaded || cambiando) return;
 
-			cambiando = true;
+            cambiando = true;
 
-			
-			if (cboFinal.SelectedIndex < cboFechaInicio.SelectedIndex)
-			{
-				cboFinal.SelectedIndex = cboFechaInicio.SelectedIndex;
-			}
 
-			cambiando = false;
+            if (cboFinal.SelectedIndex < cboFechaInicio.SelectedIndex)
+            {
+                cboFinal.SelectedIndex = cboFechaInicio.SelectedIndex;
+            }
 
-			cls.LoadDgvCatalog();
-		}
+            cambiando = false;
 
-		private void cboFinal_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			if (!isLoaded || cambiando) return;
+            cls.LoadDgvCatalog();
+        }
 
-			cambiando = true;
+        private void cboFinal_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!isLoaded || cambiando) return;
 
-			
-			if (cboFinal.SelectedIndex < cboFechaInicio.SelectedIndex)
-			{
-				cboFinal.SelectedIndex = cboFechaInicio.SelectedIndex;
-			}
+            cambiando = true;
 
-			cambiando = false;
 
-			cls.LoadDgvCatalog();
-		}
+            if (cboFinal.SelectedIndex < cboFechaInicio.SelectedIndex)
+            {
+                cboFinal.SelectedIndex = cboFechaInicio.SelectedIndex;
+            }
 
-		private void dgvCatalog_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
-		{
-			dgvCatalog.BeginInvoke(new Action(() =>
-			{
-				cls.OrdenarColumnas();
-				cls.AjustarEncabezados();
-				cls.ColorearColumnas();
-				cls.FormatearColumnasHora(dgvCatalog);
+            cambiando = false;
 
-				if (dgvCatalog.Columns.Contains(ClsObject.Column.id))
-					dgvCatalog.Columns[ClsObject.Column.id].Visible = false;
+            cls.LoadDgvCatalog();
+        }
 
-				if (dgvCatalog.Columns.Contains(ClsWorkTime.ColumnDateWtm))
-					dgvCatalog.Columns[ClsWorkTime.ColumnDateWtm].Visible = false;
+        private void dgvCatalog_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            dgvCatalog.BeginInvoke(new Action(() =>
+            {
+                cls.OrdenarColumnas();
+                cls.AjustarEncabezados();
+                cls.ColorearColumnas();
+                cls.FormatearColumnasHora(dgvCatalog);
 
-				if (dgvCatalog.Columns.Contains("d_workTime"))
-					dgvCatalog.Columns["d_workTime"].Visible = false;
+                if (dgvCatalog.Columns.Contains(ClsObject.Column.id))
+                    dgvCatalog.Columns[ClsObject.Column.id].Visible = false;
 
-				dgvCatalog.Refresh();
-			}));
-		}
-	}
+                if (dgvCatalog.Columns.Contains(ClsWorkTime.ColumnDateWtm))
+                    dgvCatalog.Columns[ClsWorkTime.ColumnDateWtm].Visible = false;
+
+                if (dgvCatalog.Columns.Contains("d_workTime"))
+                    dgvCatalog.Columns["d_workTime"].Visible = false;
+
+                dgvCatalog.Refresh();
+            }));
+        }
+
+        private void dgvCatalog_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
 
 }
