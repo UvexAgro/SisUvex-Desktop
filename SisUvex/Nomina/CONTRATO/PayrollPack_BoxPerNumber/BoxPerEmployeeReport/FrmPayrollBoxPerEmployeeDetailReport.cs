51d448719cadcfa8ecf9d1c321c528b28d29072d
DANIEL LIMON <dannyallson00@gmail.com>
2026-06-08 19:16:11 -0700

    +Frm reporte cajas agrupado por empleado (solo controles, nada de codigo)

---
+++ SisUvex/Nomina/CONTRATO/PayrollPack_BoxPerNumber/BoxPerEmployeeReport/FrmPayrollBoxPerEmployeeDetailReport.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SisUvex.Nomina.CONTRATO.PayrollPack_BoxPerNumber.BoxPerEmployeeReport
+{
+    public partial class FrmPayrollBoxPerEmployeeDetailReport : Form
+    {
+        public FrmPayrollBoxPerEmployeeDetailReport()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvReport_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
--- SisUvex/Properties/Resources.Designer.cs
+++ SisUvex/Properties/Resources.Designer.cs
@@ -513,6 +513,16 @@ namespace SisUvex.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap verIcon16 {
+            get {
+                object obj = ResourceManager.GetObject("verIcon16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap xIcon {
             get {
                 object obj = ResourceManager.GetObject("xIcon", resourceCulture);
