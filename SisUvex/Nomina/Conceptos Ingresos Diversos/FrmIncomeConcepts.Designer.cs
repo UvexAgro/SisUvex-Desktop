f819968aa637761c482333a47eb90ee4b7cbaa1e
susana2809 <susanaaguirre717@gmail.com>
2026-07-18 11:43:08 -0700

    cambie el diseño de los frm catalago y nuevo

--- SisUvex/Nomina/Conceptos Ingresos Diversos/FrmIncomeConcepts.Designer.cs
+++ SisUvex/Nomina/Conceptos Ingresos Diversos/FrmIncomeConcepts.Designer.cs
@@ -28,43 +28,62 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmIncomeConcepts));
 			DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
 			DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmIncomeConcepts));
 			btnAdd = new Button();
 			btnModify = new Button();
 			btnRemove = new Button();
 			dgvCatalog = new DataGridView();
+			panel1 = new Panel();
+			label2 = new Label();
+			label1 = new Label();
+			pictureBox1 = new PictureBox();
 			((System.ComponentModel.ISupportInitialize)dgvCatalog).BeginInit();
+			panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
 			SuspendLayout();
 			// 
 			// btnAdd
 			// 
-			btnAdd.Location = new Point(12, 12);
+			btnAdd.BackgroundImageLayout = ImageLayout.Zoom;
+			btnAdd.Image = (Image)resources.GetObject("btnAdd.Image");
+			btnAdd.ImageAlign = ContentAlignment.MiddleLeft;
+			btnAdd.Location = new Point(15, 98);
+			btnAdd.Margin = new Padding(3, 4, 3, 4);
 			btnAdd.Name = "btnAdd";
-			btnAdd.Size = new Size(75, 23);
+			btnAdd.Size = new Size(111, 31);
 			btnAdd.TabIndex = 1;
 			btnAdd.Text = "Añadir";
+			btnAdd.TextAlign = ContentAlignment.MiddleRight;
 			btnAdd.UseVisualStyleBackColor = true;
 			btnAdd.Click += btnAdd_Click;
 			// 
 			// btnModify
 			// 
-			btnModify.Location = new Point(93, 12);
+			btnModify.Image = (Image)resources.GetObject("btnModify.Image");
+			btnModify.ImageAlign = ContentAlignment.MiddleLeft;
+			btnModify.Location = new Point(132, 98);
+			btnModify.Margin = new Padding(3, 4, 3, 4);
 			btnModify.Name = "btnModify";
-			btnModify.Size = new Size(75, 23);
+			btnModify.Size = new Size(111, 31);
 			btnModify.TabIndex = 2;
 			btnModify.Text = "Modificar";
+			btnModify.TextAlign = ContentAlignment.MiddleRight;
 			btnModify.UseVisualStyleBackColor = true;
 			btnModify.Click += btnModify_Click;
 			// 
 			// btnRemove
 			// 
-			btnRemove.Location = new Point(174, 12);
+			btnRemove.Image = (Image)resources.GetObject("btnRemove.Image");
+			btnRemove.ImageAlign = ContentAlignment.MiddleLeft;
+			btnRemove.Location = new Point(249, 98);
+			btnRemove.Margin = new Padding(3, 4, 3, 4);
 			btnRemove.Name = "btnRemove";
-			btnRemove.Size = new Size(75, 23);
+			btnRemove.Size = new Size(111, 31);
 			btnRemove.TabIndex = 4;
 			btnRemove.Text = "Eliminar";
+			btnRemove.TextAlign = ContentAlignment.MiddleRight;
 			btnRemove.UseVisualStyleBackColor = true;
 			btnRemove.Click += btnRemove_Click;
 			// 
@@ -86,10 +105,12 @@
 			dataGridViewCellStyle1.SelectionForeColor = SystemColors.WindowText;
 			dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
 			dgvCatalog.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+			dgvCatalog.ColumnHeadersHeight = 29;
 			dgvCatalog.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
 			dgvCatalog.EnableHeadersVisualStyles = false;
 			dgvCatalog.ImeMode = ImeMode.NoControl;
-			dgvCatalog.Location = new Point(12, 41);
+			dgvCatalog.Location = new Point(14, 137);
+			dgvCatalog.Margin = new Padding(3, 4, 3, 4);
 			dgvCatalog.Name = "dgvCatalog";
 			dgvCatalog.ReadOnly = true;
 			dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
@@ -103,23 +124,68 @@
 			dgvCatalog.RowHeadersVisible = false;
 			dgvCatalog.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
 			dgvCatalog.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-			dgvCatalog.Size = new Size(696, 387);
+			dgvCatalog.Size = new Size(819, 434);
 			dgvCatalog.TabIndex = 11;
+			// 
+			// panel1
+			// 
+			panel1.BackColor = SystemColors.ControlLightLight;
+			panel1.Controls.Add(label2);
+			panel1.Controls.Add(label1);
+			panel1.Controls.Add(pictureBox1);
+			panel1.Location = new Point(12, 12);
+			panel1.Name = "panel1";
+			panel1.Size = new Size(797, 79);
+			panel1.TabIndex = 12;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(120, 44);
+			label2.Name = "label2";
+			label2.Size = new Size(485, 20);
+			label2.TabIndex = 2;
+			label2.Text = "Administra los conceptos utilizados para generar ingresos en la nómina.";
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Font = new Font("Segoe UI Semibold", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			label1.Location = new Point(120, 3);
+			label1.Name = "label1";
+			label1.Size = new Size(251, 31);
+			label1.TabIndex = 1;
+			label1.Text = "Conceptos de Ingresos";
+			// 
+			// pictureBox1
+			// 
+			pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+			pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+			pictureBox1.Location = new Point(3, 3);
+			pictureBox1.Name = "pictureBox1";
+			pictureBox1.Size = new Size(96, 73);
+			pictureBox1.TabIndex = 0;
+			pictureBox1.TabStop = false;
 			// 
 			// FrmIncomeConcepts
 			// 
-			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleDimensions = new SizeF(8F, 20F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(720, 454);
+			ClientSize = new Size(845, 605);
+			Controls.Add(panel1);
 			Controls.Add(dgvCatalog);
 			Controls.Add(btnRemove);
 			Controls.Add(btnModify);
 			Controls.Add(btnAdd);
 			Icon = (Icon)resources.GetObject("$this.Icon");
+			Margin = new Padding(3, 4, 3, 4);
 			Name = "FrmIncomeConcepts";
 			Text = "Conceptos";
 			Load += FrmIncomeConcepts_Load;
 			((System.ComponentModel.ISupportInitialize)dgvCatalog).EndInit();
+			panel1.ResumeLayout(false);
+			panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
 			ResumeLayout(false);
 		}
 
@@ -129,5 +195,9 @@
 		private Button btnModify;
 		private Button btnRemove;
 		public DataGridView dgvCatalog;
+		private Panel panel1;
+		private Label label1;
+		private PictureBox pictureBox1;
+		private Label label2;
 	}
 }
