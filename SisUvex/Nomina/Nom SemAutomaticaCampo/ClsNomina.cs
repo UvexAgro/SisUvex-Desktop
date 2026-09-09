002a5ca47e0447086e2e92c8db5b03894ebea164
susana <susanaaguirre717@gmail.com>
2026-09-09 09:44:52 -0700

    AGREGUE UN FORMULARIO PARA LA NOMINA DE CAMPO

---
+++ SisUvex/Nomina/Nom SemAutomaticaCampo/ClsNomina.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisUvex.Nomina.Nom_SemAutomaticaCampo
+{
+	public class ClsNomina
+	{
+		public FrmNomina frm;
+
+	}
+}
---
+++ SisUvex/Nomina/Nom SemAutomaticaCampo/FrmNomina.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SisUvex.Nomina.Ingresos_Diversos;
+
+namespace SisUvex.Nomina.Nom_SemAutomaticaCampo
+{
+	public partial class FrmNomina : Form
+	{
+		public ClsNomina cls;
+		public FrmNomina()
+		{
+			InitializeComponent();
+			cls ??= new ClsNomina();
+			cls.frm ??= this;
+		}
+
+		private void FrmNomina_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
